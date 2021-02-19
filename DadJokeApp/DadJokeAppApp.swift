f8e5fc435f5c611763622be5d7f303c718bfd22f
Russell Gordon <rgordon@lcs.on.ca>
2021-02-19 11:14:06 -0500

    Initial Commit

---
+++ DadJokeApp/DadJokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokeAppApp.swift
+//  DadJokeApp
+//
+//  Created by Russell Gordon on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct DadJokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
